3e07b61b3a7b7d73a6e0f3e20be11f4a23e993ae
IgnacioCatron <82251062+IgnacioCatron@users.noreply.github.com>
2024-07-01 10:29:09 -0400

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,20 @@ import ItemListComponent from '../components/ItemListComponent';
 import ShareComponent from '../components/ShareComponent';
 import styles from '../styles/Home.module.css';
 
-const initialState = {
+type State = {
+    items: string[];
+};
+
+type Action =
+    | { type: 'ADD_ITEM'; payload: string }
+    | { type: 'REMOVE_ITEM'; payload: number }
+    | { type: 'LOAD_ITEMS'; payload: string[] };
+
+const initialState: State = {
     items: [],
 };
 
-const reducer = (state, action) => {
+const reducer = (state: State, action: Action): State => {
     switch (action.type) {
         case 'ADD_ITEM':
             const newItems = [...state.items, action.payload];
@@ -32,15 +41,15 @@ const Home = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     useEffect(() => {
-        const savedItems = JSON.parse(localStorage.getItem('items')) || [];
+        const savedItems = JSON.parse(localStorage.getItem('items') || '[]');
         dispatch({ type: 'LOAD_ITEMS', payload: savedItems });
     }, []);
 
-    const handleAddItem = (item) => {
+    const handleAddItem = (item: string) => {
         dispatch({ type: 'ADD_ITEM', payload: item });
     };
 
-    const handleRemoveItem = (index) => {
+    const handleRemoveItem = (index: number) => {
         dispatch({ type: 'REMOVE_ITEM', payload: index });
     };
 
@@ -50,7 +59,6 @@ const Home = () => {
             <InputComponent onAddItem={handleAddItem} />
             <ItemListComponent items={state.items} onRemoveItem={handleRemoveItem} />
             <ShareComponent items={state.items} />
-
         </div>
     );
 };
